a6068c179cbafe299ba8e72ad5aa63af4e3eaefa
wumei-aka <67529960+wumei-aka@users.noreply.github.com>
2023-02-19 21:46:28 +0800

    update github

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,7 +10,6 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("push test!");
         System.out.println("pull test!");
-        System.out.println("gitee test!");
-
+        System.out.println("项目更新!");
     }
 }
